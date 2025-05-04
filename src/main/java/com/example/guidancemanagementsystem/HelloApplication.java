b6ab29eb889d9032a5e1b22a4ed83a41fe344154
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-05-04 23:00:44 +0800

    Done designing the login in HelloApplication and added images 👌

--- src/main/java/com/example/guidancemanagementsystem/HelloApplication.java
+++ src/main/java/com/example/guidancemanagementsystem/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.guidancemanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/guidancemanagementsystem/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("PEÑARANDA OFF-CAMPUS - SCHOOL GUINDANCE MANAGEMENT SYSTEM - LOGIN");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("/com/example/guidancemanagementsystem/images/logo.png")));
+        stage.setResizable(false);
+        stage.centerOnScreen();
         stage.show();
     }
 
