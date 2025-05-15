fc722fedbf09e7cefa4ea7997382a9e710416767
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-05-15 15:00:34 +0800

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.guidancemanagementsystem {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires org.xerial.sqlitejdbc;
 
 
     opens com.example.guidancemanagementsystem to javafx.fxml;
